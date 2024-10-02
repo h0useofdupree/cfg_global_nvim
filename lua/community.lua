fcbef22ef4bb80085c1d4a8fb8f93d4f71e4eb0a
h0useofdupree <joel.riekemann@gmail.com>
2024-10-02 18:30:58 +0200

    sort community plugins

--- lua/community.lua
+++ lua/community.lua
@@ -2,16 +2,28 @@
 -- We import this file in `lazy_setup.lua` before the `plugins/` folder.
 -- This guarantees that the specs are processed before any user plugins.
 
+-- #TODO: Sort by category!
 ---@type LazySpec
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.note-taking.obsidian-nvim" },
+
+  -- Colorschemes
   { import = "astrocommunity.colorscheme.everforest" },
   { import = "astrocommunity.colorscheme.catppuccin" },
-  { import = "astrocommunity.motion.leap-nvim" },
-  { import = "astrocommunity.note-taking.obsidian-nvim" },
-  -- { import = "astrocommunity.recipes.heirline-nvchad-statusline" },
+  { import = "astrocommunity.colorscheme.nordic-nvim" },
+  { import = "astrocommunity.colorscheme.onedarkpro-nvim" },
+
+  -- Recipes
   { import = "astrocommunity.recipes.neo-tree-dark" },
   { import = "astrocommunity.recipes.telescope-nvchad-theme" },
   { import = "astrocommunity.recipes.neovide" },
+  -- { import = "astrocommunity.recipes.heirline-nvchad-statusline" },
+  { import = "astrocommunity.recipes.heirline-vscode-winbar" },
+  { import = "astrocommunity.recipes.vscode-icons" },
+
+  -- Motion
+  { import = "astrocommunity.motion.leap-nvim" },
+  { import = "astrocommunity.motion.nvim-surround" },
 }
