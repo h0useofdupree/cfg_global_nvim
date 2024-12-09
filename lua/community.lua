b64c537f7d86fcaeac7814f65c7adc4de2b3867e
h0useofdupree <joel.riekemann@gmail.com>
2024-12-09 13:29:52 +0100

    misc

--- lua/community.lua
+++ lua/community.lua
@@ -26,5 +26,10 @@ return {
 
   -- Motion
   { import = "astrocommunity.motion.leap-nvim" },
+  -- { import = "astrocommunity.motion.flash-nvim"},
   { import = "astrocommunity.motion.nvim-surround" },
+  { import = "astrocommunity.motion.nvim-spider" },
+
+  -- Packs
+  { import = "astrocommunity.pack.go" },
 }
