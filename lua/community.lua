5a5ef975def19832ffa7d70dc92c2c8ce0e9aa36
h0useofdupree <joel.riekemann@gmail.com>
2024-12-15 23:17:01 +0100

    add some stuff to community

--- lua/community.lua
+++ lua/community.lua
@@ -5,7 +5,6 @@
 ---@type LazySpec
 return {
   "AstroNvim/astrocommunity",
-  { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.note-taking.obsidian-nvim" },
 
   -- Colorschemes
@@ -21,15 +20,31 @@ return {
   { import = "astrocommunity.recipes.neovide" },
   { import = "astrocommunity.recipes.heirline-nvchad-statusline" },
   { import = "astrocommunity.recipes.heirline-vscode-winbar" },
-  -- { import = "astrocommunity.recipes.vscode-icons" },
   { import = "astrocommunity.recipes.astrolsp-no-insert-inlay-hints" },
 
+  -- Icons
+  -- { import = "astrocommunity.recipes.vscode-icons" },
+  { import = "astrocommunity.icon.mini-icons" },
+
   -- Motion
-  { import = "astrocommunity.motion.leap-nvim" },
+  -- { import = "astrocommunity.motion.leap-nvim" },
   -- { import = "astrocommunity.motion.flash-nvim"},
-  { import = "astrocommunity.motion.nvim-surround" },
+  -- { import = "astrocommunity.motion.nvim-surround" },
+  -- { import = "astrocommunity.motion.mini-surround" },
   { import = "astrocommunity.motion.nvim-spider" },
+
+  -- Markdown and LaTeX
+  { import = "astrocommunity.markdown-and-latex.render-markdown-nvim" },
+  { import = "astrocommunity.markdown-and-latex.markdown-preview-nvim" },
+
+  -- Media
+  -- BUG: Does not work (failed to load config)
+  -- { import = "astrocommunity.media.image-nvim" },
 
   -- Packs
   { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.lua" },
+
+  -- Misc
+  { import = "astrocommunity.game.leetcode-nvim" },
 }
